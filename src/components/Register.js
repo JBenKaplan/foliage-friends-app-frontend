3c07cb195a5443942971effa2978fef47cc700ea
Ben Kaplan <bjminkaplan@gmail.com>
2022-11-14 08:07:57 -0600

    Working on auth

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,33 +1,81 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { RegisterUser } from '../services/Auth'
+
 const Register = () => {
+  let navigate = useNavigate()
+  const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  }
+  const [formValues, setFormValues] = useState(initialState)
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await RegisterUser({
+      name: formValues.name,
+      email: formValues.email,
+      password: formValues.password
+    })
+    setFormValues(initialState)
+    navigate('/login')
+  }
   return (
     <div>
-      <form className="signup-container">
+      <form className="signup-container" onSubmit={handleSubmit}>
         <input
+          onChange={handleChange}
           type="text"
           placeholder="name"
-          id="name-input"
+          name="name"
           className="signup-input"
+          value={formValues.name}
+          required
         ></input>
         <input
           type="text"
           placeholder="email"
-          id="email-input"
+          name="email"
           className="signup-input"
+          onChange={handleChange}
+          value={formValues.email}
+          required
         ></input>
         <input
           type="text"
           placeholder="password"
-          id="password-input"
+          name="password"
           className="signup-input"
+          onChange={handleChange}
+          value={formValues.password}
+          required
         ></input>
         <input
           type="text"
           placeholder="confirm password"
-          id="confirmpassword-input"
+          name="confirmPassword"
           className="signup-input"
+          onChange={handleChange}
+          value={formValues.confirmPassword}
+          required
         ></input>
 
-        <button className="signup-btn">Sign Up</button>
+        <button
+          disabled={
+            !formValues.email ||
+            (!formValues.password &&
+              formValues.confirmPassword === formValues.password)
+          }
+          className="signup-btn"
+        >
+          Sign Up
+        </button>
       </form>
     </div>
   )
