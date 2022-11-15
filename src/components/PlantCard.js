46803a008e3e1bb1290e5ac284e0becac23286cd
Marissa Dilig <hello.maripd@gmail.com>
2022-11-15 15:49:13 -0600

    updated plant card

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,17 +1,33 @@
-import { useNavigate } from 'react-router-dom'
 import { useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import Client from '../services/api'
 
 const PlantCard = (props) => {
-  const navigate = useNavigate()
+  const [currentRoomName, setRoomName] = useState()
+  const [currentPlantName, setPlantName] = useState()
+  const [currentImage, setImage] = useState()
+
   let id = useParams()
 
-  const plantCardHandleClick = () => {
-    navigate(`/plantcard/${props.plant.image}`)
-  }
+  useEffect(() => {
+    const getData = async () => {
+      if (id == !undefined) {
+        let response = await Client.get(`/plantid/:plant_id/GetPlantId/${id}`)
+        console.log(response.data.plant)
+        setRoomName(response.data.roomId)
+        setPlantName(response.data.name)
+        setImage(response.data.image)
+      }
+    }
+    getData()
+  }, [])
+
   return (
     <div className="plantcard-container">
-      <h1 className="plantname-text">Plant Card</h1>
-      <img className="plantcard-image" src="" />
+      <h1 className="room-title">{currentRoomName}</h1>
+      <h4 className="plantname-text">{currentPlantName}</h4>
+      <img className="plantcard-image" src={currentImage} />
     </div>
   )
 }
