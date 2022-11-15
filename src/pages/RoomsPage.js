f0e9f1d2231c67959ca911fa60dcfae7ad7ca6c1
Ben Kaplan <bjminkaplan@gmail.com>
2022-11-15 11:27:32 -0600

    Added room delete page

--- src/pages/RoomsPage.js
+++ src/pages/RoomsPage.js
@@ -31,17 +31,22 @@ const RoomsPage = ({ user }) => {
   }, [user])
 
   return (
-    <div className="main-container">
+    <div className="rooms-container">
       <h3>Edit Rooms</h3>
       <div className="roomList">
         {rooms.map((room) => (
           <div key={room._id} className="roomInfo">
             <h3>{room.name}</h3>
-            <button onClick={() => RemoveRoom(room.id)}>X</button>
+            <button
+              className="removeRoomBtn"
+              onClick={() => RemoveRoom(room.id)}
+            >
+              X
+            </button>
           </div>
         ))}
-        <button onClick={() => navigate(-1)}>Back</button>
       </div>
+      <button onClick={() => navigate(-1)}>Back</button>
     </div>
   )
 }
