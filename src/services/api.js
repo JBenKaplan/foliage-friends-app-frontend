64d9141bafe5b0d370c0a11c5010c6f68a2d7166
Ben Kaplan <bjminkaplan@gmail.com>
2022-11-16 14:05:17 -0600

    Changed deployment base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'http://localhost:3001'
+export const BASE_URL = 'https://git.heroku.com/foliage-friends-database.git'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
