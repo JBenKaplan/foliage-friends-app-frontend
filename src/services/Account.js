5716166d800cce206a184771d61736068b41df04
Josh Levine <levine.joshuaadam@gmail.com>
2022-11-16 12:01:50 -0500

    delete user cascade bug fixed

--- src/services/Account.js
+++ src/services/Account.js
@@ -2,7 +2,7 @@ import Client from './api'
 
 export const DeleteAccount = async (userId) => {
   try {
-    const res = await Client.delete(`/users/delete/${userId}`)
+    const res = await Client.delete(`/users/user/${userId}`)
     return res.data
   } catch (error) {
     throw error
